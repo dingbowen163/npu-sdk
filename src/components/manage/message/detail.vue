1331ca47b8ba6d5635ccabf103ff0a93bdecafce
丁博雯 <dingbowen@jingdata.com>
2020-05-28 19:12:55 +0800

    111

--- src/components/manage/message/detail.vue
+++ src/components/manage/message/detail.vue
@@ -11,21 +11,70 @@
     </header>
     <div class="card">
       <div class="message-item">
-        <div></div>
-        <div>
-          <div class="message-title">
-            <span class="username">用户名</span>
-            <el-button class="reply-btn" type="primary" icon="el-icon-edit">回复</el-button>
+        <div class="question">
+          <div class="left-img">
+            <i class="el-icon-chat-dot-round"></i>
           </div>
-          <div class="question-content">记得放个假哦而无需 v 分？</div>
+          <div class="fr right-con">
+            <div class="message-title">
+              <div class="fl username">
+                用户1
+                <span class="text">发表提问：</span>
+              </div>
+              <div class="fr">
+                <span class="publish-date">2020-5-10 23:34:21</span>
+              </div>
+            </div>
+            <div class="question-content">记得放个假哦而无需 v 分？</div>
+            <el-button
+              class="reply-btn hang-btn"
+              type="text"
+              icon="el-icon-arrow-down"
+              @click="hideReplyInp"
+              v-if="showReply"
+            >收起</el-button>
+            <el-button class="reply-btn" type="text" icon="el-icon-edit" @click="openReplyInp">回复(1)</el-button>
+          </div>
         </div>
-        <el-input
-          type="textarea"
-          placeholder="请输入内容"
-          v-model="replyContent"
-          maxlength="30"
-          show-word-limit
-        ></el-input>
+        <div class="reply-box">
+          <el-collapse-transition>
+            <el-input
+              v-if="showReply"
+              class="reply-textarea"
+              type="textarea"
+              placeholder="请输入回复内容"
+              v-model="replyContent"
+              maxlength="30"
+              show-word-limit
+            ></el-input>
+          </el-collapse-transition>
+          <ul class="reply-list">
+            <li class="reply-con">
+              <div class="message-title">
+                <div class="fl username">
+                  客服1
+                  <span class="text">回复：</span>
+                </div>
+                <div class="fr">
+                  <span class="publish-date repay-date">2020-5-10 23:34:21</span>
+                </div>
+              </div>
+              <div class="question-content">记得放个假哦而无需 v 分？</div>
+            </li>
+            <li class="reply-con">
+              <div class="message-title">
+                <div class="fl username">
+                  客服1
+                  <span class="text">回复：</span>
+                </div>
+                <div class="fr">
+                  <span class="publish-date repay-date">2020-5-10 23:34:21</span>
+                </div>
+              </div>
+              <div class="question-content">记得放个假哦而无需 v 分？</div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -34,9 +83,19 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      replyContent: "",
+      showReply: false
+    };
   },
-  methods: {},
+  methods: {
+    openReplyInp() {
+      this.showReply = true;
+    },
+    hideReplyInp() {
+      this.showReply = false;
+    }
+  },
   components: {},
   mounted() {}
 };
